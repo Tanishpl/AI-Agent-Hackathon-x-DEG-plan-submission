e0f2546e178e1f5db021dd57097ad3226460679d
Talen Mudaly <talen.mudaly@student.manchester.ac.uk>
2025-11-25 06:27:40 +0000

    implemented dotenv support

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api import router as api_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI(title="Project Reflex Backend")
 
